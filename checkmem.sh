136a5091c292a8856aebef6901e1dcf19f44e7a0
kingsae <kingsae1004@gmail.com>
2023-01-13 15:01:36 +0900

    remove file

--- checkmem.sh
+++
@@ -1,6 +0,0 @@
-echo "==========================================================="
-echo "Memory usage @ kingsae"
-echo "==========================================================="
-cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi | sed 's/ kB/G/g' | grep MemTotal
-echo "==========================================================="
-ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
