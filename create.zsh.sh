31ebd6caa3cca74aacd8f444b522b2230b2a6f26
kingsae <kingsae1004@gmail.com>
2023-01-16 16:10:36 +0900

    Create zsh script

---
+++ create.zsh.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+source ~/.zshrc
